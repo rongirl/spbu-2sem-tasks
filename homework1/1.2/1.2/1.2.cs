c68ebae75feecf119d43f48ae1f2e3d1c3f0de23
rongirl <gro.pretty.arabella@mail.ru>
2022-02-26 15:56:08 +0300

    Реализовала BurrowsWheelerTransform()

---
+++ homework1/1.2/1.2/1.2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BurrowsWheelerTransform
+{
+    class Program
+    {
+        private static string BurrowsWheelerTransform(string originalString, ref int number)
+        {
+            string[] tableOfStrings = new string[originalString.Length];
+            for (int i = 0; i < originalString.Length; i++)
+            {
+                tableOfStrings[i] = originalString;
+                char symbol = originalString[originalString.Length - 1];
+                originalString = originalString.Remove(originalString.Length - 1, 1);
+                originalString = symbol + originalString;
+            }
+            Array.Sort(tableOfStrings);
+            string resultString = "";
+            for (int i = 0; i < tableOfStrings.Length; i++)
+            {
+                resultString += tableOfStrings[i][originalString.Length - 1];
+                if (string.Compare(tableOfStrings[i], originalString) == 0)
+                {
+                    number = i;
+                }
+            }
+            return resultString;
+        }
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("{0} {1}", BurrowsWheelerTransform("abcdef", ref number), number);
+            //Console.WriteLine(number);
+        }
+    }
+}
