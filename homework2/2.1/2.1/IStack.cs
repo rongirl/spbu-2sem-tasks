454ee90cc4724bafdbda37ee68b5b0bb164574b4
rongirl <gro.pretty.arabella@mail.ru>
2022-03-07 14:51:57 +0300

    Реализовала интерфейс

---
+++ homework2/2.1/2.1/IStack.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._1
+{
+    internal interface IStack
+    {
+        void Push(int value);
+
+        int Pop();
+
+        bool IsEmpty();
+
+        void DeleteStack();
+    }
+}
