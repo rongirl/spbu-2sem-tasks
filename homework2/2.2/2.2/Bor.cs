3857d3f019ebbee69553f2056eea075a09dc9b98
rongirl <gro.pretty.arabella@mail.ru>
2022-03-11 15:21:46 +0300

    inital commit

---
+++ homework2/2.2/2.2/Bor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BorSpace;
+
+public class Bor
+{
+    private class Node
+    {
+        public Node[] next = new Node[26];
+        public bool isTerminal;
+        public int countStringsContainPrefix = 0;
+    }
+    private int size = 0;
+    private Node root = new Node();
+
+}
