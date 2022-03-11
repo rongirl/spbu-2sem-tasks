96877b62fa7453edaf245a7a4c96a59ebe8bdde8
rongirl <gro.pretty.arabella@mail.ru>
2022-03-11 17:23:52 +0300

    Реализовала Remove(), Add(), Contain()

--- homework2/2.2/2.2/Bor.cs
+++ homework2/2.2/2.2/Bor.cs
@@ -16,5 +16,90 @@ public class Bor
     }
     private int size = 0;
     private Node root = new Node();
+    public bool Add(string element)
+    {   
+        if (Contain(element))
+        {
+            return false;
+        }
+        Node current = root;
+        for (int i = 0; i < element.Length; i++)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+            char symbol = element[i];
+            if (current.next[symbol - 'a'] == null)
+            {
+                current.next[symbol - 'a'] = new Node();
+            }
+            if (current != null)
+            {
+                current = current.next[symbol - 'a'];
+                current.countStringsContainPrefix++;
+            }
+        }
+        if (current != null)
+        {
+            current.isTerminal = true;
+        }
+        size++;
+        return true;
+    }
+    public bool Contain(string element)
+    {
+        Node current = root;
+        for (int i = 0; i < element.Length; i++)
+        {
+            current = current.next[element[i] - 'a'];
+            if (current == null)
+            {
+                return false;
+            }
+        }
+        return current.isTerminal;
+    }
+    
+    public bool Remove(string element)
+    {
+        if (!Contain(element))
+        {
+            return false;
+        }
+        Node current = root;
+        for (int i = 0; i < element.Length; i++)
+        {
+            if (current == null)
+            {
+                return false;
+            }
+            char symbol = element[i];
+            if (current.next[symbol - 'a'] == null)
+            {
+                return false;
+            }
+            if (current.next[symbol - 'a'].countStringsContainPrefix > 1)
+            {
+                current.next[symbol - 'a'].countStringsContainPrefix--;
+                current = current.next[symbol - 'a'];
+            }
+            else
+            {
+                var temporary = current.next[symbol - 'a'];
+                current.next[symbol - 'a'] = null;
+                current = temporary;
 
+            }
+        }
+        size--;
+        return true;
+    }
+
+    static void Main(string[] args)
+    {
+        Bor bor = new Bor();
+        bor.Add("mik");
+        bor.Remove("mik");
+    }
 }
