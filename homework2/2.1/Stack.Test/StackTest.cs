3e5b92f6c8dd530b6a93c19f2e16fa59d1216a8a
rongirl <gro.pretty.arabella@mail.ru>
2022-03-16 17:35:10 +0300

    Реализовала PushAndPopShallGetExpectedValue(), PushAndPopShallLeaveStackEmpty(), PushThenStackIsNotEmpty(), Push_1_2_3_Pop_3_2_1()

--- homework2/2.1/Stack.Test/StackTest.cs
+++ homework2/2.1/Stack.Test/StackTest.cs
@@ -1,6 +1,10 @@
 using NUnit.Framework;
 using _2._1;
 using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace StackTest
 {
@@ -12,4 +16,42 @@ namespace StackTest
              new TestCaseData(new StackArray()),
              new TestCaseData(new StackList())
         };
+
+        [Test, TestCaseSource(nameof(Stacks))]
+        public void PushThenStackIsNotEmpty(IStack stack)
+        {
+            var result = stack.IsEmpty();
+            stack.Push(1);
+            result = result && !stack.IsEmpty();
+            Assert.IsTrue(result);
+        }
+
+        [Test, TestCaseSource(nameof(Stacks))]
+        public void PushAndPopShallGetExpectedValue(IStack stack)
+        {
+            stack.Push(1);
+            var value = stack.Pop();
+            Assert.AreEqual(1, value);
+        }
+
+        [Test, TestCaseSource(nameof(Stacks))]
+        public void PushAndPopShallLeaveStackEmpty(IStack stack)
+        {
+            stack.Push(1);
+            stack.Pop();
+            Assert.IsTrue(stack.IsEmpty());
+        }
+
+        [Test, TestCaseSource(nameof(Stacks))]
+        public void Push_1_2_3_Pop_3_2_1(IStack stack)
+        {
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Assert.AreEqual(3, stack.Pop());
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+        }
     }
+}
+
