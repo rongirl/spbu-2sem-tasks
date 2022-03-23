07951b4f81cea542af734ffe8cefa190d5d7cfc0
rongirl <gro.pretty.arabella@mail.ru>
2022-03-23 14:58:36 +0300

    Inital commit

---
+++ homework4/4.1/4.1/INode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1;
+public interface INode
+{
+    void Print();
+
+    int Calculate();
+
+}
---
+++ homework4/4.1/4.1/Operand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1;
+
+public class Operand : INode
+{
+    private int Value { get; set; }
+
+    public void Print()
+    {
+        Console.Write($"{Value}");
+    }
+
+    public int Calculate() => Value;
+}
+
