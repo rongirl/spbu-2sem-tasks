bb12aeb6a38031cfc91219971af968d0a9ae296d
rongirl <gro.pretty.arabella@mail.ru>
2022-03-23 18:33:55 +0300

    Inital commit

--- homework4/4.1/4.1/INode.cs
+++ homework4/4.1/4.1/INode.cs
@@ -10,5 +10,4 @@ public interface INode
     void Print();
 
     int Calculate();
-
 }
---
+++ homework4/4.1/4.1/Tree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._1;
+public class Tree
+{
+    public int getNumber(string inputString, int index)
+    {
+        int number = 0;
+        int sign = 1;
+        if (inputString[index] == '-')
+        {
+            sign = -1;
+            index++;
+        }
+        while (index < inputString.Length && inputString[index] >= '0' && inputString[index] <= '9')
+        {
+            index++;
+            number = number * 10 + (int)inputString[index];
+        }
+        return number * sign;
+    }
+    public INode createNewNode()
+}
