48444df2c17f66e9fea2d4a4a84a432b7bfbaa79
rongirl <gro.pretty.arabella@mail.ru>
2022-03-24 22:02:49 +0300

    Inital commit

---
+++ homework4/4.2/4.2/List.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._2;
+
+public class List
+{
+    public void Add(int value, int position)
+
+    public void Remove(int value, int position)
+
+}
