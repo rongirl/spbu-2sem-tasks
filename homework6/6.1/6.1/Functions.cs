542291e9d80c9b0930966841a9e23fe23f2c50b8
rongirl <gro.pretty.arabella@mail.ru>
2022-04-11 00:05:58 +0300

    Реализовала Map(), Filter()

---
+++ homework6/6.1/6.1/Functions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1;
+
+public class Functions
+{
+    List<int>Map(List<int> list, Func<int, int> function)
+    {
+        List<int> result = new List<int>();
+        foreach (var i in list)
+        {
+            result.Add(function(i));
+        }
+        return result;
+    }
+
+    List<int>Filter(List<int> list, Func<int, bool> function)
+    {
+        List<int> result = new List<int>();
+        foreach (var i in list)
+        {
+            if (function(i))
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+
+    //int Fold
+}
