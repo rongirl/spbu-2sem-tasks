0c1378bbebf38cd6f133e29a78a2fa26a29731e2
rongirl <gro.pretty.arabella@mail.ru>
2022-04-11 14:52:41 +0300

    Реализовала функцию Fold(), написала комментарии, написала тесты

---
+++ homework6/6.1/6.1.Test/FunctionsTest.cs
@@ -0,0 +1,41 @@
+using NUnit.Framework;
+using Task6_1;
+using System.Collections.Generic;
+
+namespace FunctionsTest;
+
+public class Tests
+{   
+    private List<int> list = new List<int>() { 1, 2, 3, 4 };
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+    
+
+    [Test]
+    public void MapTest()
+    {
+        Assert.AreEqual(Functions.Map(list, x => x * 2), new List<int>() { 2, 4, 6, 8 });
+        Assert.AreEqual(Functions.Map(list, x => x * x), new List<int>() { 1, 4, 9, 16 });
+        Assert.AreEqual(Functions.Map(list, x => x - 10), new List<int>() { -9, -8, -7, -6 });
+    }
+
+    [Test]
+    public void FilterTest()
+    {
+        Assert.AreEqual(Functions.Filter(list, x => x % 2 == 0), new List<int>() { 2, 4 });
+        Assert.AreEqual(Functions.Filter(list, x => x % 2 == 1), new List<int>() { 1, 3 });
+        Assert.AreEqual(Functions.Filter(list, x => x > 10), new List<int>() { });
+    }
+
+    [Test]
+    public void FoldTest()
+    {
+        Assert.AreEqual(Functions.Fold(list, 1, (x, y) => x + y), 11);
+        Assert.AreEqual(Functions.Fold(list, 1, (x, y) => x * y), 24);
+        Assert.AreEqual(Functions.Fold(list, 1, (x, y) => x - y), -9);
+    }
+}
+
--- homework6/6.1/6.1/Functions.cs
+++ homework6/6.1/6.1/Functions.cs
@@ -4,32 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _6._1;
+namespace Task6_1;
 
 public class Functions
 {
-    List<int>Map(List<int> list, Func<int, int> function)
+    /// <summary>
+    /// Функция принимает список и функцию, преобразующую элементы списка
+    /// </summary>
+    /// <param name="list">Исходный список</param>
+    /// <param name="function">Функция преобразования элементов исходного списка</param>
+    /// <returns>Список элементов после преобразований</returns>
+    public static List<int>Map(List<int> list, Func<int, int> function)
     {
         List<int> result = new List<int>();
-        foreach (var i in list)
+        foreach (var element in list)
         {
-            result.Add(function(i));
+            result.Add(function(element));
         }
         return result;
     }
-
-    List<int>Filter(List<int> list, Func<int, bool> function)
+    /// <summary>
+    /// Функция создает новый список из элементов,
+    /// для которых переданная функция возвращает true
+    /// </summary>
+    /// <param name="list">Исходный список</param>
+    /// <param name="function">Переданная функция</param>
+    /// <returns>Список элементов, для которых переданная функция возвратила true</returns>
+    public static List<int>Filter(List<int> list, Func<int, bool> function)
     {
         List<int> result = new List<int>();
-        foreach (var i in list)
+        foreach (var element in list)
         {
-            if (function(i))
+            if (function(element))
             {
-                result.Add(i);
+                result.Add(element);
             }
         }
         return result;
     }
-
-    //int Fold
+    /// <summary>
+    /// Функция накапливает значение при прохождении списка
+    /// </summary>
+    /// <param name="list">Исходный список</param>
+    /// <param name="start">Начальное значение</param>
+    /// <param name="function"> Функция,которая берёт текущее накопленное значение и текущий элемент списка,
+    ///и возвращает следующее накопленное значение</param>
+    /// <returns>Накопленное значение</returns>
+    public static int Fold(List<int> list, int start, Func<int, int, int> function)
+    {
+        foreach (var element in list)
+        {
+            start = function(start, element);
+        }
+        return start;
+    }
 }
