6d5d8efd9abf274cc5edbc2be7b99d6997f85b5a
rongirl <gro.pretty.arabella@mail.ru>
2022-04-20 11:56:36 +0300

    Реализовала игру

--- homework6/6.2/6.2/Program.cs
+++ homework6/6.2/6.2/Program.cs
@@ -8,5 +8,25 @@ namespace Task6_2;
 
 public class Program
 {
+    static void Main(string[] args)
+    {
+      
+        try
+        {
+            var eventLoop = new EventLoop();
+            Game game = new Game();
+            game.PrintMap("C:/Users/Acer/source/repos/spbu-2sem-tasks/homework6/6.2/6.2/map.txt");
+            eventLoop.LeftHandler += game.OnLeft;
+            eventLoop.RightHandler += game.OnRight;
+            eventLoop.UpHandler += game.OnUp;
+            eventLoop.DownHandler += game.OnDown;
 
+            eventLoop.Run();
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+
+    }
 }
