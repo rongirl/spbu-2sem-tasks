76ee8bf73fb26a14b2316e1ec9c77cd3b9ccf4f4
rongirl <gro.pretty.arabella@mail.ru>
2022-05-19 17:01:36 +0300

    Inital commit

---
+++ ControlTest/ControlTest/BubbleSortTest/BubbleSortTest1.cs
@@ -0,0 +1,33 @@
+using NUnit.Framework;
+using Sorting;
+using System.Collections.Generic;
+
+namespace BubbleSortTest;
+
+public class Tests
+{ 
+    static object[] Cases =
+    {
+        new object[] { new List<int>(), new List<int>() }, 
+        new object[] { new List<int> { 7, 6, 10, 3, 5, 8, 7, 99 }, new List<int> { 3, 5, 6, 7, 7, 8, 10, 99 } },
+        new object[] { new List<int> { 1, 2, 3 }, new List<int> { 1, 2, 3 } }
+    };
+
+    [TestCaseSource(nameof(Cases))]
+    public void BubbleSortTest(List<int> list, List<int> expectedList)
+    {   
+        var comparer = Comparer<int>.Default;   
+        Sort.BubbleSort(list, comparer);
+        Assert.AreEqual(expectedList, list);
+    }
+
+    [Test]
+    public void BubbleSortListStringsTest()
+    {
+        var list = new List<string> { "kkk", "bc", "abd" };
+        var expectedList = new List<string> { "abd", "bc", "kkk" };
+        var comparer = Comparer<string>.Default;
+        Sort.BubbleSort(list, comparer);
+        Assert.AreEqual(expectedList, list);
+    }
+}
