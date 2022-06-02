b54ead74463943db64153e1ea80ce28cef5bc42b
rongirl <gro.pretty.arabella@mail.ru>
2022-06-02 12:23:38 +0300

    Fixed

--- homework2/2.1/2.1/StackArray.cs
+++ homework2/2.1/2.1/StackArray.cs
@@ -1,15 +1,32 @@
 ﻿namespace Task2_1;
 
+/// <summary>
+/// Стек, основанный на массиве
+/// </summary>
 public class StackArray : IStack
-{
+{   
+    /// <summary>
+    /// Массив элементов
+    /// </summary>
     private double[] stackElements;
+
+    /// <summary>
+    /// Размер стека
+    /// </summary>
     private int size;
 
+    /// <summary>
+    /// Инициализация стека
+    /// </summary>
     public StackArray()
     {
         stackElements = new double[10];
     }
 
+    /// <summary>
+    /// Добавляет элемент в голову стека
+    /// </summary>
+    /// <param name="value">Добавляемое значение</param>
     public void Push(double value)
     {
         if (size == stackElements.Length)
@@ -20,14 +37,25 @@ public class StackArray : IStack
         size++;
     }
 
+    /// <summary>
+    /// Увеличение размера массива
+    /// </summary>
     public void Resize()
     {
         Array.Resize(ref stackElements, stackElements.Length * 2);
     }
 
+    /// <summary>
+    /// Проверка на пустоту
+    /// </summary>
+    /// <returns>True, если пуст
+    /// False, если нет</returns>
     public bool IsEmpty()
        => size == 0;
 
+    /// <summary>
+    /// Возвращает значение с головы стека и удаляет его
+    /// </summary>
     public double Pop()
     {
         if (IsEmpty())
--- homework2/2.1/2.1/StackList.cs
+++ homework2/2.1/2.1/StackList.cs
@@ -1,19 +1,44 @@
 ﻿namespace Task2_1;
 
+/// <summary>
+/// Стек, основанный на списке
+/// </summary>
 public class StackList : IStack
-{
+{   
+    /// <summary>
+    /// Верхний элемент стека
+    /// </summary>
     private StackElement? head;
+
+    /// <summary>
+    /// Класс элемента из стека, 
+    /// состоящий из значения элемента,
+    /// ссылки на следующий элемент
+    /// </summary>
     private class StackElement
-    {
+    {  
         public double value;
         public StackElement? next;
     }
-    public bool IsEmpty()
-        => head == null;    
 
+    /// <summary>
+    /// Проверка на пустоту
+    /// </summary>
+    /// <returns>True, если пуст
+    /// False, если нет</returns>
+    public bool IsEmpty()
+        => head == null;
+
+    /// <summary>
+    /// Добавляет элемент в голову стека
+    /// </summary>
+    /// <param name="value">Добавляемое значение</param>
     public void Push(double value)
         => head = new StackElement { value = value, next = head };
 
+    /// <summary>
+    /// Возвращает значение с головы стека и удаляет его
+    /// </summary>
     public double Pop()
     {
         if (IsEmpty())
